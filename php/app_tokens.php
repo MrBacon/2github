c08b0ef95f301b9baaeb531e8b1e8f538e730018
Mr Bacon <MrBacon@outlook.com>
2013-12-05 07:29:10 +0100

    1st

---
+++ php/app_tokens.php
@@ -0,0 +1,9 @@
+<?php
+
+// OAuth tokens for single-user programming
+$consumer_key = 	"UZq9C3TS0ynFEOzhM8iGRw";
+$consumer_secret = 	"CVFd3qvLkr4hz73RhhwJRqUN0gh2DUcXlIxg2b8";
+$user_token = 		"1535653465-tSIZt2YS0oM6kWL2ofYTvtiFLR9VsvCvBDTNjlE";
+$user_secret = 		"fepm9x9C6BCBZHNs710Jyl1V7VqF0mKWjHLG4VhR2ZPfv";
+
+?>
