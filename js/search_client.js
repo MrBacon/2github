6b89805672e14d2bb166715e4e89a2555b55d856
Mr Bacon <MrBacon@outlook.com>
2013-12-05 14:34:37 +0100

    Changed the Comments.

--- js/search_client.js
+++ js/search_client.js
@@ -8,12 +8,9 @@ jQuery(document).ready(function($) {
 		$('#search_results').html('<img src="image/ajax_loader.gif"> Searching Twitter...');
 		
 		// Get the value of the input field
-		// Encode it for use in a URL
 		var search_value = encodeURIComponent($('input[name=search_terms]').val());
 		
 		// Send the search terms to the server in an Ajax request
-		// This URL is for illustration only
-		// You MUST change it to your server
 		$.ajax({
 			url: './php/search_server.php?q=' + search_value,
 			success: function(data){
